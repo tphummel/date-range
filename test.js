d745d6c76d2746fffa213c83ebec1b0b9f7bf626
Tom Hummel <tphummel@gmail.com>
2018-01-14 14:53:32 -0800

    check end date in addition to start date in "ten day" test

--- test.js
+++ test.js
@@ -7,6 +7,7 @@ const tenDays = lib({startDate: '2017-01-01', endDate: '2017-01-10'})
 tap.ok(Array.isArray(tenDays))
 tap.ok(tenDays.length === 10)
 tap.ok(tenDays[0] === '2017-01-01')
+tap.ok(tenDays[tenDays.length - 1] === '2017-01-10')
 
 const singleDay = lib({startDate: '2017-01-01', endDate: '2017-01-01'})
 tap.ok(Array.isArray(singleDay))
