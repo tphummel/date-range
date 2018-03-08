06da7ab304b1141fc39818f223ffd2a29efe2797
Tom Hummel <tphummel@gmail.com>
2018-03-07 21:46:40 -0800

    move existing tests into separate test blocks

--- test.js
+++ test.js
@@ -3,13 +3,19 @@
 const tap = require('tap')
 const lib = require('./index.js')
 
-const tenDays = lib({startDate: '2017-01-01', endDate: '2017-01-10'})
-tap.ok(Array.isArray(tenDays))
-tap.ok(tenDays.length === 10)
-tap.ok(tenDays[0] === '2017-01-01')
-tap.ok(tenDays[tenDays.length - 1] === '2017-01-10')
+tap.test('ten days', (t) => {
+  const tenDays = lib({startDate: '2017-01-01', endDate: '2017-01-10'})
+  t.ok(Array.isArray(tenDays))
+  t.ok(tenDays.length === 10)
+  t.ok(tenDays[0] === '2017-01-01')
+  t.ok(tenDays[tenDays.length - 1] === '2017-01-10')
+  t.end()
+})
 
-const singleDay = lib({startDate: '2017-01-01', endDate: '2017-01-01'})
-tap.ok(Array.isArray(singleDay))
-tap.ok(singleDay.length === 1)
-tap.ok(singleDay[0] === '2017-01-01')
+tap.test('single day', (t) => {
+  const singleDay = lib({startDate: '2017-01-01', endDate: '2017-01-01'})
+  t.ok(Array.isArray(singleDay))
+  t.ok(singleDay.length === 1)
+  t.ok(singleDay[0] === '2017-01-01')
+  t.end()
+})
